7238faa2e0c463356dcaafdac0e5fbec2bbc4a4c
Mia Wong <miaw629@gmail.com>
2020-05-25 16:37:29 -0400

    rm cached

--- src/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import authReducer from "./auth/authReducer";
-import recipeReducer from "./recipes/recipeReducer";
-
-export default combineReducers({
-    authReducer,
-    recipeReducer,
-});
