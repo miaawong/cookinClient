794c4a511fbc81dac861a759c9c8b7db0cfb5e99
Mia Wong <miaw629@gmail.com>
2020-05-26 17:19:08 -0400

    add some styling to recipeCard

--- src/recipes/components/recipe/RecipeCard.js
+++ src/recipes/components/recipe/RecipeCard.js
@@ -2,48 +2,94 @@ import React from "react";
 import { connect, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
-import { getCurrentRecipe } from "../../recipeAction";
+import { device } from "../../../Theme";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
 const Main = styled.div`
-    margin: 2rem 6rem 2rem 2rem;
+    font-family: ${(props) => props.theme.font};
+    margin: 0 auto;
     display: flex;
     flex-wrap: wrap;
-    justify-content: space-around;
+    justify-content: start;
+
+    @media ${device.full} {
+        margin: 0 6rem 0 2rem;
+    }
 `;
 
 const CardBox = styled.div`
-    border: 2px solid black;
-    width: 30rem;
-    height: 50rem;
-    margin: 1rem;
-    flex: 1 1 30rem;
-`;
-const Image = styled.img`
-    width: 100%;
-    height: 70%;
-    object-fit: cover;
-`;
-const Description = styled.div`
+    background: #f8f8f8;
+    margin: ${({ recipes }) => (recipes.length === 1 ? "5px 0" : "5px auto")};
+    width: 40rem;
+    height: 32rem;
     display: flex;
     flex-wrap: wrap;
+    position: relative;
+    @media ${device.small} {
+        width: 22rem;
+        height: 28rem;
+    }
+    @media ${device.full} {
+        flex-wrap: ${({ recipes }) =>
+            recipes.length === 1 ? "nowrap" : "wrap"};
+    }
+    & > * {
+        flex: 2 1;
+    }
+`;
+const Image = styled.img`
+    object-fit: cover;
+    width: 100%;
+    height: 65%;
+    @media ${device.full} {
+        width: ${({ recipes }) => (recipes.length === 1 ? "65%" : "100%")};
+        height: ${({ recipes }) => (recipes.length === 1 ? "100%" : "64%")};
+    }
+`;
+
+const Description = styled.div`
+    padding: 1rem 1rem 0 1rem;
+    height: 35%;
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: space-between;
+
+    flex: 1 1 auto;
+    & > h1 {
+        margin: 0;
+        font-size: ${(props) => props.theme.fontSizes.large};
+    }
 `;
 
 const RecipeCard = ({ recipes }) => {
-    console.log(recipes, "recipes");
-    const { img, recipeName, recipeDesc, duration } = recipes.map((recipe) => {
-        return recipe;
-    });
-    console.log(img, "img");
-    return (
-        <Main>
-            <CardBox>
-                <Image src={img} />
+    let card = recipes.map((recipe) => {
+        const { img, recipeName, recipeDesc, duration } = recipe;
+        return (
+            <CardBox recipes={recipes}>
+                <Image src={img} recipes={recipes} />
+                <FaRegHeart
+                    style={{
+                        position: "absolute",
+                        color: "white",
+                        right: "1rem",
+                        top: "1rem",
+                    }}
+                    size={30}
+                />
+                {/* onclick change icon to <FaHeart/> */}
+
                 <Description>
-                    <h1>{recipeName}</h1>
+                    <div style={{ width: "100%" }}>
+                        <h1>{recipeName}</h1>
+                        <p>1hr 30mins</p>
+                    </div>
                 </Description>
+                <p>{recipeDesc}</p>
             </CardBox>
-        </Main>
-    );
+        );
+    });
+    return <Main>{card}</Main>;
 };
 
 export default RecipeCard;
