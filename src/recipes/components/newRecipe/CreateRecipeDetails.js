6d9d4c89c217b95658323fb9ee6f2be8c1546b6f
Mia Wong <miaw629@gmail.com>
2020-06-01 17:50:53 -0400

    add styling

--- src/recipes/components/newRecipe/CreateRecipeDetails.js
+++ src/recipes/components/newRecipe/CreateRecipeDetails.js
@@ -1,19 +1,10 @@
 import React from "react";
 import { connect, useDispatch } from "react-redux";
+import styled from "styled-components";
 import { useForm } from "react-hook-form";
 import { setDraftRecipe } from "../../recipeAction";
-import { Grommet, Form, FormField, TextInput } from "grommet";
-const grommetTheme = {
-    global: {
-        font: {
-            family: "Roboto",
-            size: "24px",
-        },
-        colors: {
-            focus: "#ffda0b",
-        },
-    },
-};
+import { StyledForm, Submit } from "./StyledForm";
+import { Grommet, TextInput, Box } from "grommet";
 
 const CreateRecipeDetails = ({}) => {
     const { register, handleSubmit, errors } = useForm();
@@ -22,40 +13,58 @@ const CreateRecipeDetails = ({}) => {
         dispatch(setDraftRecipe(data));
     };
     return (
-        <Grommet full theme={grommetTheme}>
-            <Form
-                pad="large"
-                style={{
-                    width: "80%",
-                    height: "80%",
-                    margin: "0 auto",
-                    fontFamily: `${(props) => props.theme.font}`,
-                }}
-                onSubmit={handleSubmit(onSubmit)}
+        <StyledForm onSubmit={handleSubmit(onSubmit)}>
+            <h1>Details</h1>
+            <TextInput
+                type="text"
+                name="recipeName"
+                placeholder="Recipe Name"
+                ref={register({
+                    required: "I cannot be empty",
+                })}
+            />
+            <br></br>
+            {errors["recipeName"] && <p>{errors["recipeName"].message}</p>}
+
+            <TextInput
+                type="text"
+                name="recipeDesc"
+                placeholder="Description"
+                ref={register}
+            />
+            <br></br>
+            {errors["recipeDesc"] && <p>{errors["recipeDesc"].message}</p>}
+            <TextInput
+                type="text"
+                name="servings"
+                placeholder="Servings"
+                ref={register({
+                    pattern: {
+                        value: /^(0|[1-9][0-9]*)$/,
+                        message: "must be a number",
+                    },
+                })}
+            />
+            <br></br>
+            {errors["servings"] && <p>{errors["servings"].message}</p>}
+
+            <Box
+                direction="row-responsive"
+                gap="large"
+                // style={{
+                //     display: "flex",
+                //     width: "100%",
+                //     justifyContent: "space-between",
+                //     alignContent: "space-between",
+
+                //     flexWrap: "no-wrap",
+                // }}
             >
                 <TextInput
-                    type="text"
-                    name="recipeName"
-                    placeholder="Recipe Name"
-                    ref={register({
-                        required: "I cannot be empty",
-                    })}
-                />
-                <br></br>
-                {errors["recipeName"] && <p>{errors["recipeName"].message}</p>}
-
-                <TextInput
-                    type="text"
-                    name="recipeDesc"
-                    placeholder="Description"
-                    ref={register}
-                />
-                <br></br>
-                {errors["recipeDesc"] && <p>{errors["recipeDesc"].message}</p>}
-                <TextInput
-                    type="text"
-                    name="servings"
-                    placeholder="servings"
+                    style={{ width: "50%" }}
+                    type="number"
+                    name="duration_hour"
+                    placeholder="Hour"
                     ref={register({
                         pattern: {
                             value: /^(0|[1-9][0-9]*)$/,
@@ -63,59 +72,43 @@ const CreateRecipeDetails = ({}) => {
                         },
                     })}
                 />
-                <br></br>
-                {errors["servings"] && <p>{errors["servings"].message}</p>}
 
-                <div
-                    style={{
-                        display: "flex",
-                        width: "100%",
-                        justifyContent: "space-between",
-                    }}
-                >
-                    <TextInput
-                        style={{ width: "90%", marginRight: "1rem" }}
-                        type="number"
-                        name="duration_hour"
-                        placeholder="hour"
-                        ref={register({
-                            pattern: {
-                                value: /^(0|[1-9][0-9]*)$/,
-                                message: "must be a number",
-                            },
-                        })}
-                    />
-                    <br></br>
-                    {errors["duration_hour"] && (
-                        <p>{errors["duration_hour"].message}</p>
-                    )}
-                    <TextInput
-                        style={{ width: "90%", marginLeft: "1rem" }}
-                        type="number"
-                        name="duration_mins"
-                        placeholder="mins"
-                        ref={register({
-                            pattern: {
-                                value: /^(0|[1-9][0-9]*)$/,
-                                message: "must be a number",
-                            },
-                        })}
-                    />
-                    {errors["duration_mins"] && (
-                        <p>{errors["duration_mins"].message}</p>
-                    )}
-                </div>
                 <br></br>
+                {errors["duration_hour"] && (
+                    <p>{errors["duration_hour"].message}</p>
+                )}
                 <TextInput
-                    type="text"
-                    name="img"
-                    placeholder="Image"
-                    ref={register}
+                    style={{ width: "50%" }}
+                    type="number"
+                    name="duration_mins"
+                    placeholder="Mins"
+                    ref={register({
+                        pattern: {
+                            value: /^(0|[1-9][0-9]*)$/,
+                            message: "must be a number",
+                        },
+                    })}
                 />
-                <br></br>
-                <input type="submit" />
-            </Form>
-        </Grommet>
+
+                {errors["duration_mins"] && (
+                    <p>{errors["duration_mins"].message}</p>
+                )}
+            </Box>
+            <br></br>
+            <TextInput
+                type="text"
+                name="img"
+                placeholder="Image"
+                ref={register}
+            />
+            <br></br>
+            <div>
+                <Submit type="submit" value="Submit">
+                    {" "}
+                    Submit
+                </Submit>
+            </div>
+        </StyledForm>
     );
 };
 
