5ded35402762b8f518c5655495fba770581b4d38
Mia Wong <miaw629@gmail.com>
2020-06-01 17:50:34 -0400

    add styling

--- src/recipes/components/newRecipe/CreateIngredients.js
+++ src/recipes/components/newRecipe/CreateIngredients.js
@@ -2,6 +2,8 @@ import React from "react";
 import { connect, useDispatch } from "react-redux";
 import { useForm, useFieldArray } from "react-hook-form";
 import { setDraftRecipe } from "../../recipeAction";
+import { StyledForm, Submit } from "./StyledForm";
+import { TextInput, Select } from "grommet";
 
 const CreateIngredients = ({ draftRecipe }) => {
     const dispatch = useDispatch();
@@ -19,54 +21,51 @@ const CreateIngredients = ({ draftRecipe }) => {
         dispatch(setDraftRecipe(draftRecipe));
     };
     return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                {fields.map((input, index) => {
-                    return (
-                        <div key={input.id}>
-                            <input
-                                type="text"
-                                name={`ingredients[${index}].ingName`}
-                                placeholder="ingredient"
-                                ref={register()}
-                            />
-                            <input
-                                type="Number"
-                                name={`ingredients[${index}].amount`}
-                                placeholder="how much?"
-                                ref={register()}
-                            />
-                            <select
-                                name={`ingredients[${index}].unit`}
-                                ref={register()}
-                            >
-                                <option value=""></option>
-                                <option value="tsp">tsp</option>
-                                <option value="tbsp">tbsp</option>
-                                <option value="cup">cup</option>
-                                <option value="oz">oz</option>
-                                <option value="lb">lb</option>
-                                <option value="g">g</option>
-                                <option value="kg">kg</option>
-                                <option value="mL">mL</option>
-                                <option value="L">L</option>
-                            </select>
-                        </div>
-                    );
-                })}
-                <button
-                    onClick={(e) => {
-                        e.preventDefault();
-                        append({ ingredients: "ingredients" });
-                    }}
-                ></button>
-                <input type="submit" />
-                <br></br>
-                {/* {errors["ingredients"] && (
+        <StyledForm onSubmit={handleSubmit(onSubmit)}>
+            {fields.map((input, index) => {
+                return (
+                    <div key={input.id}>
+                        <TextInput
+                            type="text"
+                            name={`ingredients[${index}].ingName`}
+                            placeholder="ingredient"
+                            ref={register()}
+                        />
+                        <TextInput
+                            type="Number"
+                            name={`ingredients[${index}].amount`}
+                            placeholder="how much?"
+                            ref={register()}
+                        />
+                        <Select
+                            name={`ingredients[${index}].unit`}
+                            ref={register()}
+                        >
+                            <option value="tsp">tsp</option>
+                            <option value="tbsp">tbsp</option>
+                            <option value="cup">cup</option>
+                            <option value="oz">oz</option>
+                            <option value="lb">lb</option>
+                            <option value="g">g</option>
+                            <option value="kg">kg</option>
+                            <option value="mL">mL</option>
+                            <option value="L">L</option>
+                        </Select>
+                    </div>
+                );
+            })}
+            <button
+                onClick={(e) => {
+                    e.preventDefault();
+                    append({ ingredients: "ingredients" });
+                }}
+            ></button>
+            <Submit type="submit"> Submit</Submit>
+            <br></br>
+            {/* {errors["ingredients"] && (
                         <p>{errors["ingredients"].message}</p>
                     )} */}
-            </form>
-        </div>
+        </StyledForm>
     );
 };
 
