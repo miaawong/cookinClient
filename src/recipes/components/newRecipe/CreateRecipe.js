7cf260c66e143231ace2c79f2061fd2100c388ee
Mia Wong <miaw629@gmail.com>
2020-06-01 17:50:46 -0400

    add grommet styling

--- src/recipes/components/newRecipe/CreateRecipe.js
+++ src/recipes/components/newRecipe/CreateRecipe.js
@@ -3,14 +3,47 @@ import { connect } from "react-redux";
 import CreateRecipeDetails from "./CreateRecipeDetails";
 import CreateIngredients from "./CreateIngredients";
 import CreateDirections from "./CreateDirections";
+import { Grommet } from "grommet";
+const grommetTheme = {
+    global: {
+        font: {
+            family: "Roboto",
+            size: "24px",
+        },
+        colors: {
+            focus: "#ffda0b",
+        },
+    },
+};
 
 const CreateRecipe = ({ JWToken, draftRecipeProgress }) => {
     if (draftRecipeProgress === 0) {
-        return <CreateRecipeDetails />;
+        return (
+            <Grommet
+                theme={grommetTheme}
+                style={{ height: "90%", display: "flex", alignItems: "center" }}
+            >
+                <CreateRecipeDetails />
+            </Grommet>
+        );
     } else if (draftRecipeProgress === 1) {
-        return <CreateIngredients />;
+        return (
+            <Grommet
+                theme={grommetTheme}
+                style={{ height: "90%", display: "flex", alignItems: "center" }}
+            >
+                <CreateIngredients />
+            </Grommet>
+        );
     } else if (draftRecipeProgress === 2) {
-        return <CreateDirections />;
+        return (
+            <Grommet
+                theme={grommetTheme}
+                style={{ height: "90%", display: "flex", alignItems: "center" }}
+            >
+                <CreateDirections />
+            </Grommet>
+        );
     }
 };
 
