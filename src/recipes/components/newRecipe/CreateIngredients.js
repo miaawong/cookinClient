98bcd6a3db6db8755ba4f7ec585d182193a30f28
Mia Wong <miaw629@gmail.com>
2020-06-02 18:57:13 -0400

    add options with controller, other(txt input)

--- src/recipes/components/newRecipe/CreateIngredients.js
+++ src/recipes/components/newRecipe/CreateIngredients.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect, useDispatch } from "react-redux";
-import { useForm, useFieldArray } from "react-hook-form";
+import { useForm, useFieldArray, Controller } from "react-hook-form";
 import { setDraftRecipe } from "../../recipeAction";
 import { StyledForm, Submit } from "./StyledForm";
 import { TextInput, Select } from "grommet";
 
 const CreateIngredients = ({ draftRecipe }) => {
+    const [options, setOptions] = useState([
+        "tsp",
+        "tbsp",
+        "cup",
+        "oz",
+        "lb",
+        "g",
+        "kg",
+        "ml",
+        "l",
+        "other",
+    ]);
     const dispatch = useDispatch();
-    const { register, handleSubmit, errors, control, formState } = useForm({
+    const {
+        unregister,
+        register,
+        handleSubmit,
+        errors,
+        control,
+        formState,
+        setValue,
+        getValues,
+        watch,
+    } = useForm({
         defaultValues: {
             ingredients: [{ ingName: "", amount: null, unit: "" }],
         },
@@ -20,9 +42,22 @@ const CreateIngredients = ({ draftRecipe }) => {
         draftRecipe.ingredients = data.ingredients;
         dispatch(setDraftRecipe(draftRecipe));
     };
+    // const [otherOption, setOtherOption] = useState(false);
+    const [newOpt, setNew] = useState("");
+    const handleChange = (e) => {
+        if (e.value === "other") {
+            return newOpt;
+        } else {
+            return e.value;
+        }
+    };
+    console.log(options);
+
     return (
         <StyledForm onSubmit={handleSubmit(onSubmit)}>
             {fields.map((input, index) => {
+                const unit = watch(`ingredients[${index}].unit`);
+
                 return (
                     <div key={input.id}>
                         <TextInput
@@ -37,20 +72,28 @@ const CreateIngredients = ({ draftRecipe }) => {
                             placeholder="how much?"
                             ref={register()}
                         />
-                        <Select
+
+                        <Controller
+                            as={<Select value="" options={options} />}
                             name={`ingredients[${index}].unit`}
-                            ref={register()}
+                            control={control}
+                            onChange={([e]) => handleChange(e)}
+                        />
+
+                        <TextInput
+                            name="otherOptions"
+                            ref={register}
+                            onChange={(event) => setNew(event.target.value)}
+                            value={newOpt}
+                        />
+                        <button
+                            onClick={(e) => {
+                                e.preventDefault();
+                                setOptions([...options, newOpt]);
+                            }}
                         >
-                            <option value="tsp">tsp</option>
-                            <option value="tbsp">tbsp</option>
-                            <option value="cup">cup</option>
-                            <option value="oz">oz</option>
-                            <option value="lb">lb</option>
-                            <option value="g">g</option>
-                            <option value="kg">kg</option>
-                            <option value="mL">mL</option>
-                            <option value="L">L</option>
-                        </Select>
+                            hi
+                        </button>
                     </div>
                 );
             })}
