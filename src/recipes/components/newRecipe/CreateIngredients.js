046ae7fa591971567241078912e90890d0a515b4
Mia Wong <miaw629@gmail.com>
2020-06-03 18:56:05 -0400

    add styling to ingredients

--- src/recipes/components/newRecipe/CreateIngredients.js
+++ src/recipes/components/newRecipe/CreateIngredients.js
@@ -3,7 +3,21 @@ import { connect, useDispatch } from "react-redux";
 import { useForm, useFieldArray, Controller } from "react-hook-form";
 import { setDraftRecipe } from "../../recipeAction";
 import { StyledForm, Submit } from "./StyledForm";
-import { TextInput, Select } from "grommet";
+import { TextInput, Select, Box, Keyboard } from "grommet";
+import styled from "styled-components";
+import { FaPlus } from "react-icons/fa";
+
+const Ingredient = styled.div`
+    display: flex;
+    flex-wrap: ${({ unit }) => (unit === "other" ? "wrap" : "no-wrap")};
+    justify-content: space-evenly;
+    align-items: center;
+`;
+
+const AddIngredients = styled.button`
+    height: 3.5rem;
+    background-color: #000;
+`;
 
 const CreateIngredients = ({ draftRecipe }) => {
     const [options, setOptions] = useState([
@@ -20,21 +34,18 @@ const CreateIngredients = ({ draftRecipe }) => {
     ]);
     const dispatch = useDispatch();
     const {
-        unregister,
         register,
         handleSubmit,
-        errors,
+        // errors,
         control,
-        formState,
         setValue,
-        getValues,
         watch,
     } = useForm({
         defaultValues: {
             ingredients: [{ ingName: "", amount: null, unit: "" }],
         },
     });
-    const { fields, append, remove, insert } = useFieldArray({
+    const { fields, append } = useFieldArray({
         control,
         name: "ingredients",
     });
@@ -42,13 +53,8 @@ const CreateIngredients = ({ draftRecipe }) => {
         draftRecipe.ingredients = data.ingredients;
         dispatch(setDraftRecipe(draftRecipe));
     };
-    // const [otherOption, setOtherOption] = useState(false);
+
     const [option, setOpt] = useState("");
-    const handleChange = (e) => {
-        return e.value;
-    };
-    console.log(options);
-    console.log(option);
 
     return (
         <StyledForm onSubmit={handleSubmit(onSubmit)}>
@@ -56,50 +62,70 @@ const CreateIngredients = ({ draftRecipe }) => {
                 const unit = watch(`ingredients[${index}].unit`);
 
                 return (
-                    <div key={input.id}>
-                        <TextInput
-                            type="text"
-                            name={`ingredients[${index}].ingName`}
-                            placeholder="ingredient"
-                            ref={register()}
-                        />
-                        <TextInput
-                            type="Number"
-                            name={`ingredients[${index}].amount`}
-                            placeholder="how much?"
-                            ref={register()}
-                        />
+                    <Ingredient key={input.id} unit={unit}>
+                        <Box direction="row-responsive" align="center">
+                            <TextInput
+                                type="text"
+                                name={`ingredients[${index}].ingName`}
+                                placeholder="ingredient"
+                                ref={register()}
+                            />
+                            <TextInput
+                                type="Number"
+                                name={`ingredients[${index}].amount`}
+                                placeholder="how much?"
+                                ref={register()}
+                            />
 
-                        <Controller
-                            as={<Select options={options} value={option} />}
-                            name={`ingredients[${index}].unit`}
-                            control={control}
-                            onChange={([e]) => handleChange(e)}
-                        />
+                            <Controller
+                                as={
+                                    <Select
+                                        options={options}
+                                        value={option}
+                                        dropHeight="small"
+                                    />
+                                }
+                                name={`ingredients[${index}].unit`}
+                                control={control}
+                                onChange={([e]) => e.value}
+                            />
 
-                        <TextInput
-                            name="otherOptions"
-                            ref={register}
-                            onChange={(event) => setOpt(event.target.value)}
-                        />
-                        <button
-                            onClick={(e) => {
-                                e.preventDefault();
-                                setOptions([...options, option]);
-                                setValue(`ingredients[${index}].unit`, option);
-                            }}
-                        >
-                            hi
-                        </button>
-                    </div>
+                            <AddIngredients
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    append({ ingredients: "ingredients" });
+                                }}
+                            >
+                                <FaPlus style={{ color: "white" }} size={22} />
+                            </AddIngredients>
+                        </Box>
+
+                        {unit === "other" && (
+                            <Keyboard
+                                style={{ width: "100%" }}
+                                onEnter={(e) => {
+                                    e.preventDefault();
+                                    setOptions([...options, option]);
+                                    setValue(
+                                        `ingredients[${index}].unit`,
+                                        option
+                                    );
+                                }}
+                            >
+                                <TextInput
+                                    style={{ margin: "1rem 0 " }}
+                                    placeholder="new option"
+                                    name="otherOptions"
+                                    ref={register}
+                                    onChange={(event) =>
+                                        setOpt(event.target.value)
+                                    }
+                                />
+                            </Keyboard>
+                        )}
+                    </Ingredient>
                 );
             })}
-            <button
-                onClick={(e) => {
-                    e.preventDefault();
-                    append({ ingredients: "ingredients" });
-                }}
-            ></button>
             <Submit type="submit"> Submit</Submit>
             <br></br>
             {/* {errors["ingredients"] && (
