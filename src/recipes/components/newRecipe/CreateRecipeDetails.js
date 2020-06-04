6ceb05c78217802a45bbe7780feb3f342a1930ee
Mia Wong <miaw629@gmail.com>
2020-06-04 13:12:39 -0400

    add labels, refs for keyboard (enter)

--- src/recipes/components/newRecipe/CreateRecipeDetails.js
+++ src/recipes/components/newRecipe/CreateRecipeDetails.js
@@ -1,13 +1,18 @@
-import React from "react";
-import { connect, useDispatch } from "react-redux";
-import styled from "styled-components";
+import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 import { setDraftRecipe } from "../../recipeAction";
 import { StyledForm, Submit } from "./StyledForm";
-import { Grommet, TextInput, Box } from "grommet";
+import { TextInput, Box, FormField, Keyboard } from "grommet";
 
 const CreateRecipeDetails = ({}) => {
     const { register, handleSubmit, errors } = useForm();
+    const recipeNameRef = useRef();
+    const recipeDescRef = useRef();
+    const servingsRef = useRef();
+    const hourRef = useRef();
+    const minutesRef = useRef();
+    const imageRef = useRef();
     const dispatch = useDispatch();
     const onSubmit = (data) => {
         dispatch(setDraftRecipe(data));
@@ -15,58 +20,100 @@ const CreateRecipeDetails = ({}) => {
     return (
         <StyledForm onSubmit={handleSubmit(onSubmit)}>
             <h1>Details</h1>
-
-            <TextInput
-                type="text"
-                name="recipeName"
-                placeholder="Recipe Name"
-                ref={register({
-                    required: "I cannot be empty",
-                })}
-            ></TextInput>
+            <Keyboard
+                onEnter={(e) => {
+                    e.preventDefault();
+                    recipeDescRef.current.focus();
+                }}
+            >
+                <FormField label="Name">
+                    <TextInput
+                        type="text"
+                        name="recipeName"
+                        placeholder="Recipe Name"
+                        ref={(e) => {
+                            register(e);
+                            recipeNameRef.current = e;
+                        }}
+                    ></TextInput>
+                </FormField>
+            </Keyboard>
             {errors["recipeName"] && <p>{errors["recipeName"].message}</p>}
-
-            <TextInput
-                type="text"
-                name="recipeDesc"
-                placeholder="Description"
-                ref={register}
-            />
+            <Keyboard
+                onEnter={(e) => {
+                    e.preventDefault();
+                    servingsRef.current.focus();
+                }}
+            >
+                <FormField label="Description">
+                    <TextInput
+                        type="text"
+                        name="recipeDesc"
+                        placeholder="Description"
+                        ref={(e) => {
+                            register(e);
+                            recipeDescRef.current = e;
+                        }}
+                    />
+                </FormField>
+            </Keyboard>
             {errors["recipeDesc"] && <p>{errors["recipeDesc"].message}</p>}
-
-            <TextInput
-                type="text"
-                name="servings"
-                placeholder="Servings"
-                ref={register({
-                    pattern: {
-                        value: /^(0|[1-9][0-9]*)$/,
-                        message: "must be a number",
-                    },
-                })}
-            />
+            <Keyboard
+                onEnter={(e) => {
+                    e.preventDefault();
+                    hourRef.current.focus();
+                }}
+            >
+                <FormField label="Servings">
+                    <TextInput
+                        type="text"
+                        name="servings"
+                        placeholder="Servings"
+                        ref={(e) => {
+                            register({
+                                pattern: {
+                                    value: /^(0|[1-9][0-9]*)$/,
+                                    message: "must be a number",
+                                },
+                            });
+                            servingsRef.current = e;
+                        }}
+                    />
+                </FormField>
+            </Keyboard>
             {errors["servings"] && <p>{errors["servings"].message}</p>}
 
             <Box
                 direction="row-responsive"
-                justify="between"
+                gap="large"
+                justify="start"
                 align="center"
                 pad={{ right: "small" }}
             >
                 <Box direction="row-responsive" gap="small" align="center">
-                    <TextInput
-                        type="number"
-                        name="duration_hour"
-                        placeholder="Hour"
-                        ref={register({
-                            pattern: {
-                                value: /^(0|[1-9][0-9]*)$/,
-                                message: "must be a number",
-                            },
-                        })}
-                    />
-                    <span>hr</span>
-
+                    <Keyboard
+                        onEnter={(e) => {
+                            e.preventDefault();
+                            minutesRef.current.focus();
+                        }}
+                    >
+                        <FormField label="Hour">
+                            <TextInput
+                                type="number"
+                                name="duration_hour"
+                                placeholder="Hour"
+                                ref={(e) => {
+                                    register({
+                                        pattern: {
+                                            value: /^(0|[1-9][0-9]*)$/,
+                                            message: "must be a number",
+                                        },
+                                    });
+                                    hourRef.current = e;
+                                }}
+                            />
+                        </FormField>
+                    </Keyboard>
                     {errors["duration_hour"] && (
                         <p style={{ padding: 0, margin: 0 }}>
                             {errors["duration_hour"].message}
@@ -74,30 +121,55 @@ const CreateRecipeDetails = ({}) => {
                     )}
                 </Box>
                 <Box direction="row-responsive" gap="small" align="center">
-                    <TextInput
-                        type="number"
-                        name="duration_mins"
-                        placeholder="Mins"
-                        ref={register({
-                            pattern: {
-                                value: /^(0|[1-9][0-9]*)$/,
-                                message: "must be a number",
-                            },
-                        })}
-                    />
-                    <span>mins</span>
+                    <Keyboard
+                        onEnter={(e) => {
+                            e.preventDefault();
+                            imageRef.current.focus();
+                        }}
+                    >
+                        <FormField label="Minutes">
+                            <TextInput
+                                type="number"
+                                name="duration_mins"
+                                placeholder="Mins"
+                                ref={(e) => {
+                                    register({
+                                        pattern: {
+                                            value: /^(0|[1-9][0-9]*)$/,
+                                            message: "must be a number",
+                                        },
+                                    });
+                                    minutesRef.current = e;
+                                }}
+                            />
+                        </FormField>
+                    </Keyboard>
+
                     {errors["duration_mins"] && (
                         <p>{errors["duration_mins"].message}</p>
                     )}
                 </Box>
             </Box>
 
-            <TextInput
-                type="text"
-                name="img"
-                placeholder="Image"
-                ref={register}
-            />
+            {/* <Keyboard
+                onEnter={(e) => {
+                    e.preventDefault();
+                    submitRef.current.focus();
+                }}
+            > */}
+
+            <FormField label="Image">
+                <TextInput
+                    type="text"
+                    name="img"
+                    placeholder="Image"
+                    ref={(e) => {
+                        register(e);
+                        imageRef.current = e;
+                    }}
+                />
+            </FormField>
+            {/* </Keyboard> */}
 
             <div>
                 <Submit type="submit" value="Submit">
