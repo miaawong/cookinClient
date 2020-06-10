ad259ed771d3e4fd9ea5dadc0b460b431c4a6b8b
Mia Wong <miaw629@gmail.com>
2020-06-10 15:10:28 -0400

    add styles, dropzone height is small to drop

--- src/recipes/components/newRecipe/CreateRecipeDetails.js
+++ src/recipes/components/newRecipe/CreateRecipeDetails.js
@@ -1,19 +1,22 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useCallback } from "react";
 import { connect, useDispatch } from "react-redux";
 import { useForm } from "react-hook-form";
 import { setDraftRecipe, uploadImage } from "../../recipeAction";
 import { StyledForm, Submit } from "../../../StyledForm";
-import { TextInput, Box, FormField, Keyboard } from "grommet";
+import { TextInput, Box, FormField, Keyboard, TextArea } from "grommet";
 import { useDropzone } from "react-dropzone";
 import styled from "styled-components";
 import blankImage from "../../../images/blankimage.jpg";
 
-const Dropzone = styled.section`
-    margin: 1rem auto;
+const ImageUpload = styled.div`
+    z-index: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background: black;
+    color: white;
     width: 100%;
-    height: 40%;
-    border: 2px dashed black;
-    padding: 2rem;
+    height: 10rem;
     text-align: center;
 `;
 
@@ -21,6 +24,16 @@ const CreateRecipeDetails = ({ JWToken }) => {
     const { register, handleSubmit, errors } = useForm();
     const [file, setFile] = useState({});
     const [dropped, setDropped] = useState(false);
+    console.log(dropped, "dropped");
+    const onDrop = useCallback(
+        (acceptedFiles, e) => {
+            console.log(e, "e");
+            console.log(acceptedFiles);
+            setFile(acceptedFiles);
+            setDropped(true);
+        },
+        [file]
+    );
 
     const {
         acceptedFiles,
@@ -30,37 +43,34 @@ const CreateRecipeDetails = ({ JWToken }) => {
         isDragReject,
         isDragActive,
     } = useDropzone({
-        onDrop: (acceptedFiles) => {
-            setFile(acceptedFiles);
-            setDropped(true);
-        },
+        onDrop,
     });
 
     const filepath = acceptedFiles.map((file) => {
         Object.assign(file, {
             preview: URL.createObjectURL(file),
         });
-        console.log(file.preview);
+
         return (
-            // <li key={file.path} style={{ listStyle: "none" }}>
-            <div
-                style={{
-                    display: "flex",
-                    justifyContent: "space-around",
-                    textAlign: "center",
-                    padding: "0 2rem",
-                }}
-            >
-                <img
-                    src={file.preview}
+            <li key={file.path} style={{ listStyle: "none" }}>
+                <div
                     style={{
-                        width: "100px",
-                        height: "100px",
+                        display: "flex",
+                        justifyContent: "space-around",
+                        textAlign: "center",
                     }}
-                />
-                <p>{file.path}</p>
-            </div>
-            // </li>
+                >
+                    <img
+                        src={file.preview}
+                        style={{
+                            width: "75px",
+                            height: "75px",
+                            objectFit: "contain",
+                        }}
+                    />
+                    <p>{file.path}</p>
+                </div>
+            </li>
         );
     });
 
@@ -90,7 +100,6 @@ const CreateRecipeDetails = ({ JWToken }) => {
     };
     return (
         <StyledForm onSubmit={handleSubmit(onSubmit)}>
-            <h1>Details</h1>
             <Keyboard
                 onEnter={(e) => {
                     e.preventDefault();
@@ -103,32 +112,27 @@ const CreateRecipeDetails = ({ JWToken }) => {
                         name="recipeName"
                         placeholder="Recipe Name"
                         ref={(e) => {
-                            register(e);
+                            register(e, {
+                                required: "Name is required",
+                            });
                             recipeNameRef.current = e;
                         }}
                     ></TextInput>
                 </FormField>
             </Keyboard>
             {errors["recipeName"] && <p>{errors["recipeName"].message}</p>}
-            <Keyboard
-                onEnter={(e) => {
-                    e.preventDefault();
-                    servingsRef.current.focus();
-                }}
-            >
-                <FormField label="Description">
-                    <TextInput
-                        type="text"
-                        name="recipeDesc"
-                        placeholder="Description"
-                        ref={(e) => {
-                            register(e);
-                            recipeDescRef.current = e;
-                        }}
-                    />
-                </FormField>
-            </Keyboard>
-            {errors["recipeDesc"] && <p>{errors["recipeDesc"].message}</p>}
+            <FormField label="Description">
+                <TextArea
+                    type="text"
+                    name="recipeDesc"
+                    placeholder="Description"
+                    ref={(e) => {
+                        register(e);
+                        recipeDescRef.current = e;
+                    }}
+                    style={{ height: "8rem" }}
+                />
+            </FormField>
             <Keyboard
                 onEnter={(e) => {
                     e.preventDefault();
@@ -137,23 +141,16 @@ const CreateRecipeDetails = ({ JWToken }) => {
             >
                 <FormField label="Servings">
                     <TextInput
-                        type="text"
+                        type="number"
                         name="servings"
                         placeholder="Servings"
                         ref={(e) => {
-                            register(e, {
-                                pattern: {
-                                    value: /^(0|[1-9][0-9]*)$/,
-                                    message: "must be a number",
-                                },
-                            });
+                            register(e);
                             servingsRef.current = e;
                         }}
                     />
                 </FormField>
             </Keyboard>
-            {errors["servings"] && <p>{errors["servings"].message}</p>}
-
             <Box
                 direction="row-responsive"
                 gap="large"
@@ -174,65 +171,34 @@ const CreateRecipeDetails = ({ JWToken }) => {
                                 name="duration_hour"
                                 placeholder="Hour"
                                 ref={(e) => {
-                                    register(e, {
-                                        pattern: {
-                                            value: /^(0|[1-9][0-9]*)$/,
-                                            message: "must be a number",
-                                        },
-                                    });
+                                    register(e);
                                     hourRef.current = e;
                                 }}
                             />
                         </FormField>
                     </Keyboard>
-                    {errors["duration_hour"] && (
-                        <p style={{ padding: 0, margin: 0 }}>
-                            {errors["duration_hour"].message}
-                        </p>
-                    )}
                 </Box>
                 <Box direction="row-responsive" gap="small" align="center">
-                    <Keyboard
-                        onEnter={(e) => {
-                            e.preventDefault();
-                            // imageRef.current.focus();
-                        }}
-                    >
-                        <FormField label="Minutes">
-                            <TextInput
-                                type="number"
-                                name="duration_mins"
-                                placeholder="Mins"
-                                ref={(e) => {
-                                    register(e, {
-                                        pattern: {
-                                            value: /^(0|[1-9][0-9]*)$/,
-                                            message: "must be a number",
-                                        },
-                                    });
-                                    minutesRef.current = e;
-                                }}
-                            />
-                        </FormField>
-                    </Keyboard>
-
-                    {errors["duration_mins"] && (
-                        <p>{errors["duration_mins"].message}</p>
-                    )}
+                    <FormField label="Minutes">
+                        <TextInput
+                            type="number"
+                            name="duration_mins"
+                            placeholder="Mins"
+                            ref={(e) => {
+                                register(e);
+                            }}
+                        />
+                    </FormField>
                 </Box>
             </Box>
-
-            <Dropzone>
-                <div {...getRootProps()}>
+            <div>
+                <ImageUpload {...getRootProps()}>
                     <input {...getInputProps()} />
 
-                    {dropped ? (
-                        <div>{filepath}</div>
-                    ) : (
-                        <p>Drop or click to upload image</p>
-                    )}
-                </div>
-            </Dropzone>
+                    <p>Upload Image</p>
+                    <div>{filepath}</div>
+                </ImageUpload>
+            </div>
 
             <div>
                 <Submit type="submit" value="Submit" name="Submit">
