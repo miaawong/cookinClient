1be2b61906690f136b23299906a973033e7f401d
Mia Wong <miaw629@gmail.com>
2020-06-15 18:24:54 -0400

    remove grommet

--- src/pages/CreateRecipe.js
+++ src/pages/CreateRecipe.js
@@ -3,53 +3,7 @@ import { connect, useDispatch } from "react-redux";
 import CreateRecipeDetails from "../recipes/components/newRecipe/CreateRecipeDetails";
 import CreateIngredients from "../recipes/components/newRecipe/CreateIngredients";
 import CreateDirections from "../recipes/components/newRecipe/CreateDirections";
-import { Grommet } from "grommet";
 import { reset } from "../recipes/recipeAction";
-
-const grommetTheme = {
-    global: {
-        font: {
-            family: "Roboto",
-            size: "24px",
-        },
-        colors: {
-            focus: "#ffda0b",
-            selected: "#ffda0b",
-        },
-        selected: {
-            color: "#000000",
-        },
-        hover: {
-            background: "#ffda0b",
-            color: "#000000",
-        },
-        // control: {
-        //     border: {
-        //         radius: "0",
-        //     },
-        // },
-    },
-
-    textInput: {
-        container: {
-            extend: {
-                width: "auto",
-                border: "none",
-                borderRadius: "0",
-            },
-        },
-    },
-    select: {
-        control: {
-            extend: {
-                border: "2px solid black",
-            },
-        },
-        icons: {
-            color: "#ffda0b",
-        },
-    },
-};
 
 const CreateRecipe = ({ JWToken, draftRecipeProgress }) => {
     const dispatch = useDispatch();
@@ -66,14 +20,7 @@ const CreateRecipe = ({ JWToken, draftRecipeProgress }) => {
     } else if (draftRecipeProgress === 2) {
         currentStep = <CreateDirections />;
     }
-    return (
-        <Grommet
-            theme={grommetTheme}
-            style={{ display: "flex", alignItems: "center", height: "85%" }}
-        >
-            {currentStep}
-        </Grommet>
-    );
+    return <div style={{ height: "87%" }}>{currentStep}</div>;
 };
 
 const mapStateToProps = (state) => ({
